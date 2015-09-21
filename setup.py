87a8d4e2a0c2c81dc7211ceb56c1336b9a1bff2c
RayRiver <Messanger.6085@gmail.com>
2015-09-21 12:06:46 +0900

    change setup to qt version

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from distutils.core import setup
 import py2exe  
 import sys
 
-includes = ["encodings", "encodings.*", "sip", "Tkinter"]
+includes = ["encodings", "encodings.*", "sip", "PyQt4"]
 sys.argv.append("py2exe")  
 options = {"py2exe":
                {
-                   "bundle_files": 3,
+                   "bundle_files": 1,
                    "dll_excludes":["MSVCP90.dll"],
                    "compressed": 1,
                    "optimize": 2,
+                   "includes": includes,
                }
 }
 setup(options = options,  
       zipfile=None,   
       windows = [
           {
-              "script":'main.py',
+              "script":'gui_main.py',
               "uac_info": "requireAdministrator"
           }
       ])
